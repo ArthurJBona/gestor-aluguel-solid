5fafb74af2b91dbdbe19762a78ef06a29540dfac
ArthurJBona <arthur_bona@estudante.sesisenai.org.br>
2025-10-21 19:58:25 -0300

    Etapa 4 — Implementação do Controller (DIP)
    Implementação do controller com métodos processarAluguel e listarAlugueis, métodos essenciais para o sistema. Adição extra do método de limparHistorico para função extra.

--- src/main/java/com/tecdes/aluguel/controller/AluguelController.java
+++ src/main/java/com/tecdes/aluguel/controller/AluguelController.java
@@ -1,5 +1,31 @@
 package com.tecdes.aluguel.controller;
 
+import java.util.List;
+
+import com.tecdes.aluguel.model.Aluguel;
+import com.tecdes.aluguel.repository.AluguelRepository;
+
 public class AluguelController {
-    
+    AluguelRepository repository;
+
+    public AluguelController() {
+        this.repository = new AluguelRepository();
+    }
+
+    public String processarAluguel(Aluguel aluguel) {
+        String registro = aluguel.calcular();
+        repository.salvarRegistro(registro);
+        System.out.println("Registro adicinado: " + registro); // Debug no terminal
+        return registro;
+    }
+
+    public List<String> listarAlugueis() {
+        List<String> alugueis = repository.lerRegistros();
+        System.out.println("Lista de alugueis: " + alugueis); // Debug no terminal
+        return alugueis;
+    }
+
+    public void limparHistorico() {
+        repository.limparHistorico();
+    }
 }
